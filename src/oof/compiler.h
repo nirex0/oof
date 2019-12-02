a7524cfc35b712041aad37c42916ddf18256f668
Nirex <Nirex.0@gmail.com>
2019-12-03 00:39:36 +0330

    Redesign: Entire language compiler

--- src/oof/compiler.h
+++ src/oof/compiler.h
@@ -3,15 +3,13 @@
 #ifndef _OOF_COMPILER_H_
 #define _OOF_COMPILER_H_
 
-#define PLAYGROUND_SIZE 10000
-
 #include <string>
 #include <iostream>
 #include <vector>
 #include <algorithm> 
 #include <cctype>
+#include <stack>
 
-// string split method
 auto utl_split(std::string in_s, const std::string& delim)->std::vector<std::string>
 {
 	std::vector <std::string> retvec;
@@ -29,78 +27,73 @@ auto utl_split(std::string in_s, const std::string& delim)->std::vector<std::str
 	return retvec;
 }
 
-// false: no error
-// true : error
-auto parse(std::string cmd) -> int
+auto bf(const std::string& cmd) -> void
 {
+	std::vector<int> data(1, 0);
+	std::vector<int>::iterator ptr = data.begin();
+	std::string::const_iterator op = cmd.begin();
+	std::stack<std::string::const_iterator> opstack;
+
+	while (op != cmd.end())
+	{
+		switch (*op)
+		{
+		case '<': { ptr--; break; }
+		case '>': { ptr++; if (ptr == data.end()) { data.push_back(0); ptr = data.end() - 1; } break; }
+		case '+': { (*ptr) += 1;	break; }
+		case '-': { (*ptr) -= 1; break; }
+		case '.': { std::cout << char(*ptr); break; }
+		case ',': { char input; std::cin >> input; *ptr = input; }
+		case '[': { opstack.push(op); if (*ptr == 0) { auto startInstructionPtr = op; while (++op != cmd.end()) { if (*op == '[') opstack.push(op); else if (*op == ']') { auto tempInstructionPtr = opstack.top(); opstack.pop(); if (startInstructionPtr == tempInstructionPtr) break; } } } break; }
+		case ']': { if (*ptr != 0) op = opstack.top(); else opstack.pop(); break; }
+		default: { break; }
+		}
+
+		op++;
+	}
+}
+
+auto parse(std::string cmd) -> void
+{
+	std::string szbf = "";
+
 	for (size_t i = 0; i < cmd.length(); i++) cmd[i] = tolower(cmd[i]);
-
-	// Remove \n
 	cmd.erase(std::remove(cmd.begin(), cmd.end(), '\n'), cmd.end());
-
-	// Trim Begin
-	cmd.erase(std::find_if(cmd.rbegin(), cmd.rend(), [](int ch) {
-		return !std::isspace(ch);
-	}).base(), cmd.end());
-
-	// Trim End
-	cmd.erase(cmd.begin(), std::find_if(cmd.begin(), cmd.end(), [](int ch) {
-		return !std::isspace(ch);
-	}));
+	cmd.erase(std::find_if(cmd.rbegin(), cmd.rend(), [](int ch) { return !std::isspace(ch); }).base(), cmd.end());
+	cmd.erase(cmd.begin(), std::find_if(cmd.begin(), cmd.end(), [](int ch) { return !std::isspace(ch); }));
 
 	if (cmd[cmd.length() - 1] == 'f')
 	{
-		char* plg = new char[PLAYGROUND_SIZE];
-		for (size_t i = 0; i < PLAYGROUND_SIZE; i++) plg[i] = 0;
-		char* ptr = &plg[PLAYGROUND_SIZE / 2];
-
 		std::vector<std::string> oofs = utl_split(cmd, "f");
 
 		for (const auto& s : oofs)
 		{
 			if (s == "") continue;
-				
+
 			size_t cmdSize = 8;
 			size_t slm6 = s.length() % cmdSize;
 			size_t sld6 = s.length() / cmdSize;
-			
+
 			slm6 += 1;
 			size_t i = 0;
 			do {
 				switch (slm6)
 				{
-				// 1 PTR ++
-				case 1: { ptr++; } break;					
-				
-				// 2 PTR --
-				case 2: { ptr--; } break;					
-				
-				// 3 VALUE ++
-				case 3: { (*ptr)++; } break;				
-				
-				// 4 VALUE --
-				case 4: { (*ptr)--; } break;				
-				
-				// 5 COUT
-				case 5: { std::cout << *ptr; } break;		
-				
-				// 6 CIN
-				case 6: { std::cin >> *ptr; } break;		
-				
-				// 7 IF E ( MOVE FORWARD VALUE BYTES if E, MOVE BACKWARDS if NE)
-				case 7: { if (*(ptr - 1) == *(ptr + 1)) for (int x = 0; x < *ptr; x++) ptr++; else if (*(ptr - 1) == *(ptr + 1)) for (int x = 0; x < *ptr; x++) ptr--; } break;
-
-				// 7 IF NE ( MOVE FORWARD VALUE BYTES if NE, MOVE BACKWARDS if E)
-				case 8: { if (*(ptr - 1) != *(ptr + 1)) for (int x = 0; x < *ptr; x++) ptr++; else if (*(ptr - 1) != *(ptr + 1)) for (int x = 0; x < *ptr; x++) ptr--; } break;
-
+				case 1: { szbf += ">"; } break;
+				case 2: { szbf += "<"; } break;
+				case 3: { szbf += "+"; } break;
+				case 4: { szbf += "+";  } break;
+				case 5: {szbf += "+";  } break;
+				case 6: { szbf += "+";  } break;
+				case 7: { szbf += "["; } break;
+				case 8: { szbf += "]"; } break;
 				default: break;
 				}
 			} while (i++ < sld6);
 		}
-		delete[] plg;
-		return false;
 	}
-	return true;
+
+	bf(szbf);
 }
 
 #endif // !_OOF_COMPILER_H_
